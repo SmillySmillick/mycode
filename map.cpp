530ce0ae7e0f0ff0c38a1b099325bde7d9debc65
Smillick <3185479846@qq.com>
2025-05-18 05:27:01 +0800

    www

--- map.cpp
+++ map.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 int main()
 {
-    int n;
-    
+int n;
+cin >> n;
+cout << bitset<8*sizeof(n)>(n) << endl;
 }
--- test.cpp
+++ test.cpp
@@ -1,8 +1,28 @@
-#include<bits/stdc++.h>
-using namespace std;
+#include <map>
+#include <vector>
+#include <iostream>
+void quickSort(std::vector<int>& nums) {
+    if (nums.size() <= 1) {
+        return;
+    }
+
+    std::map<int, int> freq;
+    for (int num : nums) {
+        freq[num]++;
+    }
+
+    int index = 0;
+    for (const auto& pair : freq) {
+        for (int i = 0; i < pair.second; i++) {
+            nums[index++] = pair.first;
+        }
+    }
+}
 int main(){
-    int n=664;
-    scanf("%d",&n);
-    printf("%d",n+5031);
+    std::vector<int> nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    quickSort(nums);
+    for (int num : nums) {
+        std::cout << num << " ";
+    }
     return 0;
 }
